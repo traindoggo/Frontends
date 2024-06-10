d9897366909995b2156835df51a0341c0258e5e1
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-10 21:17:13 +0900

    Top layout, Top page

---
+++ your-first-nextjs-project/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">
+        Welcome to my blog
+      </h1>
+
+      <p className="max-w-[750px] mx-auto leading-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, ex beatae
+        optio natus voluptatum adipisci ratione quo tenetur enim ea obcaecati.
+        Ea voluptate fugiat nam explicabo iste libero dolorum voluptas.
+      </p>
+    </main>
+  );
+}

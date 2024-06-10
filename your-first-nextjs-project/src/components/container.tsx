0f628c2c173d2a8edb984c28c3039deb6b716734
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-10 21:37:29 +0900

    add border

--- your-first-nextjs-project/src/components/container.tsx
+++ your-first-nextjs-project/src/components/container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-neutral-950 max-w-[1100px] mx-auto min-h-screen flex flex-col">
+    <div className="bg-neutral-950 max-w-[1100px] mx-auto min-h-screen flex flex-col border-neutral-900 border-l border-r">
       {children}
     </div>
   );

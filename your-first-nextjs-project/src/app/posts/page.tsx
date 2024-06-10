5e0244f634002fe394c1852764e9dca85c8c8ec6
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-10 21:45:42 +0900

    GET posts from external web service called https://dummyjson
    Using server components, we can GET easily posts without useEffect or
    other react hooks :^)

--- your-first-nextjs-project/src/app/posts/page.tsx
+++ your-first-nextjs-project/src/app/posts/page.tsx
@@ -1,7 +1,27 @@
-export default function page() {
+import Link from "next/link";
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+export default async function page() {
+  const resp = await fetch("https://dummyjson.com/posts?limit=10");
+  const data: { posts: Post[] } = await resp.json();
+
   return (
-    <main className="text-center pt-32 px-5">
+    <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All Posts</h1>
+
+      <ul>
+        <li>
+          {data.posts.map((post) => (
+            <li key={post.id} className="mb-3">
+              <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </li>
+      </ul>
     </main>
   );
 }

720ab21b42c9c3b5f9a1f1880a011ca2af6de0e2
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-10 21:17:24 +0900

    Top page components, container, header, footer

---
+++ your-first-nextjs-project/src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-auto">
+      <small>&copy;2024. I, traindoggo, reserve All rights. </small>
+    </footer>
+  );
+}

3e593ae86072b9b6688772474aad743aae3867c9
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-10 21:33:43 +0900

    add typehint and change font color

--- your-first-nextjs-project/src/components/container.tsx
+++ your-first-nextjs-project/src/components/container.tsx
@@ -1,6 +1,8 @@
-export default function Container({ children }) {
+import React from "react";
+
+export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-[1100px] mx-auto min-h-screen flex flex-col">
+    <div className="bg-neutral-950 max-w-[1100px] mx-auto min-h-screen flex flex-col">
       {children}
     </div>
   );

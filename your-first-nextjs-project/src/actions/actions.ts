ec45e0da0efbe4b06bd230674cffbc24c559258d
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-13 23:59:08 +0900

    prisma: add primsa singleton instance and add create post server actions

--- your-first-nextjs-project/src/actions/actions.ts
+++ your-first-nextjs-project/src/actions/actions.ts
@@ -2,13 +2,19 @@
 // this server means 'server actions' D:
 "use server";
 
+import prisma from "@/lib/db";
+
 export async function createPost(formData: FormData) {
   // typically don't want to do this 'type conversion' in typescript
   // like this... (as blah)
   const title = formData.get("title") as string;
   const body = formData.get("body") as string;
 
-  console.log(title, body);
-
   // insert into database
+  await prisma.post.create({
+    data: {
+      title: title,
+      body: body,
+    },
+  });
 }

b5167ff49354f5f6a0ca31be9c1461a493787af2
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-14 00:22:24 +0900

    add comment for post authenticated and form data validation

--- your-first-nextjs-project/src/actions/actions.ts
+++ your-first-nextjs-project/src/actions/actions.ts
@@ -3,9 +3,20 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function createPost(formData: FormData) {
+  // auth check for incoming unknown user data
+  const { isAuthenticated } = getKindeServerSession();
+  if (!(await isAuthenticated())) {
+    redirect("api/auth/login");
+  }
+
+  // and validation FormData(form data or other crazy malicious data)
+  // e.g. zod, ...
+
   // typically don't want to do this 'type conversion' in typescript
   // like this... (as blah)
   const title = formData.get("title") as string;

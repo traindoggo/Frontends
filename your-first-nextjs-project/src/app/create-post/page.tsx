7b90918cef72b9a50bb027d8307f0b13ca76a4af
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-13 23:23:32 +0900

    authenticate using @Kinde - pretty cool :^)

--- your-first-nextjs-project/src/app/create-post/page.tsx
+++ your-first-nextjs-project/src/app/create-post/page.tsx
@@ -1,7 +1,8 @@
 import { createPost } from "@/actions/actions";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import React from "react";
 
-export default function Page() {
+export default async function Page() {
   return (
     <main className="text-center p-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All Posts</h1>
@@ -19,6 +20,8 @@ export default function Page() {
           Submit
         </button>
       </form>
+
+      <LogoutLink>Log out</LogoutLink>
     </main>
   );
 }

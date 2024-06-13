fe6fef5a1c9361a7b2545a76c8efd3751d64a6a3
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-14 00:08:59 +0900

    fetch post from prisma db

--- your-first-nextjs-project/src/components/posts-list.tsx
+++ your-first-nextjs-project/src/components/posts-list.tsx
@@ -1,3 +1,4 @@
+import prisma from "@/lib/db";
 import Link from "next/link";
 import React from "react";
 
@@ -8,12 +9,11 @@ type Post = {
 
 export default async function PostsList() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const resp = await fetch("https://dummyjson.com/posts?limit=10");
-  const data: { posts: Post[] } = await resp.json();
+  const posts = await prisma.post.findMany();
 
   return (
     <ul>
-      {data.posts.map((post) => (
+      {posts.map((post) => (
         <li key={post.id} className="mb-3">
           <Link href={`/posts/${post.id}`}>{post.title}</Link>
         </li>

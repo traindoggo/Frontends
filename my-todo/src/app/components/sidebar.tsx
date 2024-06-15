f2d6bcbd415809bb26335712f2618bc6a1baba11
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-15 20:22:47 +0900

    add width

--- my-todo/src/app/components/sidebar.tsx
+++ my-todo/src/app/components/sidebar.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-
 export default function Sidebar() {
-  return <div className="flex flex-col bg-neutral-900 p-4">Sidebar</div>;
+  return (
+    <div className="flex flex-col w-[170px] bg-neutral-900 p-4">Sidebar</div>
+  );
 }

2ecf0cfe75cb1ad3790ae3582a02c99aefe98faf
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 19:24:56 +0900

    add header

--- react_hook_form_zod/src/app/components/header.tsx
+++ react_hook_form_zod/src/app/components/header.tsx
@@ -1,9 +1,23 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="flex justify-center gap-4 bg-slate-500 px-2 py-4">
-      <p>a</p>
-      <p>b</p>
-      <p>c</p>
+      <Link
+        href="/normal"
+        className={`hover:bg-neutral-900 hover:text-neutral-200
+          text-xl px-2 rounded duration-100`}
+      >
+        Normal Form
+      </Link>
+
+      <Link
+        href="/rhf"
+        className={`hover:bg-neutral-900 hover:text-neutral-200
+        text-xl px-2 rounded duration-100`}
+      >
+        ReactHookForm
+      </Link>
     </header>
   );
 }
--- react_hook_form_zod/src/app/page.tsx
+++ react_hook_form_zod/src/app/page.tsx
@@ -1,23 +1,22 @@
-export default function Home() {
-  return (
-    <main className={`grow border-slate-950 mt-10 text-xl`}>
-      <form className={`flex flex-col gap-y-4 w-[400px] mx-auto`}>
-        <input type="email" placeholder="Email" className="px-4 py-2 rounded" />
-        <input
-          type="password"
-          placeholder="password"
-          className="px-4 py-2 rounded"
-        />
-        <input
-          type="password"
-          placeholder="Confirm password"
-          className="px-4 py-2 rounded"
-        />
+"use client";
 
-        <button type="submit" className="bg-blue-500 py-2 rounded">
-          Submit
-        </button>
-      </form>
+import { useState } from "react";
+
+export default function Home() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [erros, setErrors] = useState<string[]>();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setIsSubmitting(false);
+  };
+
+  return (
+    <main className={`grow text-center border-slate-950 mt-10 text-xl`}>
+      <h1 className="text-2xl">Click Link Avobe</h1>
     </main>
   );
 }

eaf9d4c206f89247007c3231f1c9148856faac8a
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 15:10:21 +0900

    fix flex options

--- my-todo/src/app/components/inner.tsx
+++ my-todo/src/app/components/inner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Inner({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-1">{children}</div>;
+  return <div className="flex-1 flex bg-purple-800">{children}</div>;
 }

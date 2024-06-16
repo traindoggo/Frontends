353f8d6747305b9cfb030ae9d0fc7b05ca2d3859
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 14:43:00 +0900

    header: add top link

--- my-todo/src/app/components/header.tsx
+++ my-todo/src/app/components/header.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <div
+    <header
       className={`flex justify-between items-center gap-10 py-4 px-7
         border-neutral-900 border-b-2`}
     >
-      <div className="">Logo</div>
+      <Link href={"/"}>Logo</Link>
+
       <form className="flex-1 flex">
         <input
           type="search"
@@ -13,6 +16,6 @@ export default function Header() {
         />
       </form>
       <div className="">Menu</div>
-    </div>
+    </header>
   );
 }

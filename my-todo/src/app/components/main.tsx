e068e4b686d9f937d3ebb16491e80aaeeffba02d
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 17:15:17 +0900

    fix flex css

--- my-todo/src/app/components/main.tsx
+++ my-todo/src/app/components/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <main className="grow">{children}</main>;
 }
--- my-todo/src/app/components/todo-list.tsx
+++ my-todo/src/app/components/todo-list.tsx
@@ -7,11 +7,21 @@ export default async function TodoList() {
   return (
     <main className="flex flex-col mt-10">
       <ul className="flex flex-col gap-4">
-        {todos.map((todo) => (
-          <li key={todo.id} className="border-b">
-            <Link href={`/todo/${todo.id}`}>{todo.body}</Link>
-          </li>
-        ))}
+        {todos.map((todo) => {
+          if (!todo.done) {
+            return (
+              <li
+                key={todo.id}
+                className={`flex gap-4 mx-4 px-2 py-1 border-b
+                  hover:bg-neutral-800`}
+              >
+                {/* TODO: done todo with update TodoActions */}
+                <form action="">😎</form>
+                <Link href={`/todo/${todo.id}`}>{todo.body}</Link>
+              </li>
+            );
+          }
+        })}
       </ul>
     </main>
   );

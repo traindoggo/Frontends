8c5e33d20e29603ca0301f6b54f6a0723c026e63
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 21:45:30 +0900

    componentize UpdateForm
    When using prisma and react-hook-form at same time, this causes tons of
    something errors.
    so I decided to avoid this, split /todo/[id]/page and update form.

--- my-todo/src/app/todo/[id]/page.tsx
+++ my-todo/src/app/todo/[id]/page.tsx
@@ -1,3 +1,5 @@
+import UpdateTodo from "@/app/components/update-todo";
+import { todoSchema } from "@/app/lib/types";
 import prisma from "@/lib/db";
 import { notFound } from "next/navigation";
 
@@ -7,49 +9,13 @@ export default async function Todo({ params }: { params: { id: string } }) {
       id: parseInt(params.id),
     },
   });
+  if (!todo) return notFound();
 
-  if (!todo) {
-    return notFound();
+  const validatedTodo = todoSchema.safeParse(todo);
+  if (!validatedTodo.success) {
+    console.error(validatedTodo.error);
+    return;
   }
 
-  return (
-    <main className="flex flex-col items-center p-4 gap-4">
-      <header>
-        <p>Created At {todo.createdAt.toString()}</p>
-        <p>Updated At {todo.updatedAt.toString()}</p>
-      </header>
-
-      <form className="flex flex-col gap-4 mt-10">
-        {/* TODO: how to update todo(PUT or PATCH) */}
-        <input
-          type="text"
-          name="body"
-          className={`text-lg px-4 py-1 bg-neutral-800 rounded`}
-          placeholder="input your title"
-          value={todo.body}
-        />
-
-        {/* TODO: how to update todo(PUT or PATCH) */}
-        <textarea
-          name="description"
-          id="description"
-          rows={14}
-          placeholder="it's description :^)"
-          className={`text-lg px-4 py-1 bg-neutral-800 rounded`}
-        >
-          {todo.description}
-        </textarea>
-
-        <footer className="flex">
-          {/* TODO: add update todo */}
-          <button
-            className={`ml-auto border px-2 rounded
-              bg-rose-800 border-rose-800 text-neutral-100`}
-          >
-            Update
-          </button>
-        </footer>
-      </form>
-    </main>
-  );
+  return <UpdateTodo todo={validatedTodo.data} />;
 }

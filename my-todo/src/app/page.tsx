3c3819bf29ccf6ad981b506df360e62c490eac55
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 12:07:11 +0900

    top page

--- my-todo/src/app/page.tsx
+++ my-todo/src/app/page.tsx
@@ -1,7 +1,11 @@
+import TodoForm from "./components/form";
+import TodoList from "./components/todo-list";
+
 export default function Home() {
   return (
     <main className="flex flex-col flex-1 w-full p-4">
-      <h1 className="">Hello World :^)</h1>
+      <TodoForm />
+      <TodoList />
     </main>
   );
 }

123564961d4a0144791139104208bec3e26b8970
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 14:14:51 +0900

    header: add search form

--- my-todo/src/app/components/header.tsx
+++ my-todo/src/app/components/header.tsx
@@ -1,12 +1,18 @@
 export default function Header() {
   return (
     <div
-      className={`flex justify-between items-center py-4 px-7
+      className={`flex justify-between items-center gap-10 py-4 px-7
         border-neutral-900 border-b-2`}
     >
-      <div className="">Left</div>
-      <div className="">Header</div>
-      <div className="">Right</div>
+      <div className="">Logo</div>
+      <form className="flex-1 flex">
+        <input
+          type="search"
+          className={`flex-1 px-2 rounded bg-neutral-900`}
+          placeholder="search todo"
+        />
+      </form>
+      <div className="">Menu</div>
     </div>
   );
 }

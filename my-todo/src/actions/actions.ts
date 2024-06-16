2044e105383dbb1b9ffb535e1d308d2defa5cb68
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-16 14:14:22 +0900

    delete comment

--- my-todo/src/actions/actions.ts
+++ my-todo/src/actions/actions.ts
@@ -6,13 +6,11 @@ import { revalidatePath } from "next/cache";
 export async function createTodo(formData: FormData) {
   const body = formData.get("body") as string;
 
-  // insert into database
   await prisma.todo.create({
     data: {
       body: body,
     },
   });
 
-  // revalidate for slapping caching
   revalidatePath("/");
 }

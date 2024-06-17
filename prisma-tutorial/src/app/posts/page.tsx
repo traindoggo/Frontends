42294b93e0888f4bad4fa83a4de951778e3c2aed
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-17 23:15:35 +0900

    prisma query findMany

--- prisma-tutorial/src/app/posts/page.tsx
+++ prisma-tutorial/src/app/posts/page.tsx
@@ -2,16 +2,37 @@ import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
 export default async function Page() {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    where: {
+      title: {
+        endsWith: "post",
+      },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+    },
+    // beauty of pagination
+    // take: 1,
+    // skip: 1,
+  });
+
+  const postsCount = await prisma.post.count();
 
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-2xl">All Posts</h1>
+      <h1 className="text-4xl">All Posts ({postsCount})</h1>
 
       <ul className={`border-t border-b border-neutral-700 py-4`}>
         {posts.map((post) => (
           <li key={post.id} className="flex items-center justify-between px-5">
-            <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+            <Link href={`/posts/${post.slug}`} className="text-xl">
+              {post.title}
+            </Link>
           </li>
         ))}
       </ul>

6cbe50a6c037748e5f6412e0b0227315ab65ee93
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-19 23:42:17 +0900

    add user

--- prisma-tutorial/src/app/posts/page.tsx
+++ prisma-tutorial/src/app/posts/page.tsx
@@ -3,33 +3,21 @@ import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
 export default async function Page() {
-  const posts = await prisma.post.findMany({
+  const user = await prisma.user.findUnique({
     where: {
-      title: {
-        endsWith: "post",
-      },
+      email: "john@gmail.com",
     },
-    orderBy: {
-      createdAt: "asc",
+    include: {
+      posts: true,
     },
-    select: {
-      id: true,
-      title: true,
-      slug: true,
-    },
-    // beauty of pagination
-    // take: 1,
-    // skip: 1,
   });
-
-  const postsCount = await prisma.post.count();
 
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-4xl">All Posts ({postsCount})</h1>
+      <h1 className="text-4xl">All Posts ({user?.posts.length})</h1>
 
       <ul className={`border-t border-b border-neutral-700 py-4`}>
-        {posts.map((post) => (
+        {user?.posts.map((post) => (
           <li key={post.id} className="flex items-center justify-between px-5">
             <Link href={`/posts/${post.slug}`} className="text-xl">
               {post.title}

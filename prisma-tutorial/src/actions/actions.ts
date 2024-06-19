f845d50593eee909a63c11daaf535227a9c895d3
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-20 00:30:19 +0900

    prisma error handling

--- prisma-tutorial/src/actions/actions.ts
+++ prisma-tutorial/src/actions/actions.ts
@@ -3,23 +3,35 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export async function createPost(formData: FormData) {
-  await prisma.post.create({
-    data: {
-      title: formData.get("title") as string,
-      slug: (formData.get("title") as string)
-        .replace(/s+/g, "-")
-        .toLocaleLowerCase() as string,
-      content: formData.get("content") as string,
-      author: {
-        connect: {
-          email: "john@gmail.com",
+  try {
+    await prisma.post.create({
+      data: {
+        title: formData.get("title") as string,
+        slug: (formData.get("title") as string)
+          .replace(/s+/g, "-")
+          .toLocaleLowerCase() as string,
+        content: formData.get("content") as string,
+        author: {
+          connect: {
+            email: "john@gmail.com",
+          },
         },
       },
-    },
-  });
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        console.log(
+          `There is a uniue constraint violation,
+          a new user cannot be created with this email`
+        );
+      }
+    }
+  }
 
   revalidatePath("/posts");
 }

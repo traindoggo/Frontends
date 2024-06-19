38059fb0988e0b12042f011ba435dceeb29d1f74
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-20 00:30:27 +0900

    prisma cache

--- prisma-tutorial/src/app/posts/[slug]/page.tsx
+++ prisma-tutorial/src/app/posts/[slug]/page.tsx
@@ -1,11 +1,17 @@
 import { prisma } from "@/lib/prisma";
+import { unstable_cache as cache } from "next/cache";
 
-export default async function Post({ params }: { params: { slug: string } }) {
-  const post = await prisma.post.findUnique({
+const getCachedPost = cache((slug) => {
+  return prisma.post.findUnique({
     where: {
-      slug: params.slug,
+      slug,
     },
   });
+});
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  const post = await getCachedPost(params.slug);
+
   return (
     <main className="flex flex-col items-center pt-20 gap-y-10">
       <h1 className="text-3xl">{post?.title}</h1>

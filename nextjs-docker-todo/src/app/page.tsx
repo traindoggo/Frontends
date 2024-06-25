525813b0f8cd8e9573fd74e0d958f9895593cb55
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-26 00:02:00 +0900

    prisma: show all blogs

--- nextjs-docker-todo/src/app/page.tsx
+++ nextjs-docker-todo/src/app/page.tsx
@@ -1,8 +1,19 @@
-export default function Home() {
+import { prisma } from "@/lib/db";
+
+export default async function Home() {
+  const blogs = await prisma.blog.findMany({});
+
   return (
     <main className="flex-1 flex p-4 bg-neutral-950">
       <h1 className="text-2xl">
         <p>Blogger</p>
+
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog.id}>{blog.title}</li>
+          ))}
+          <li></li>
+        </ul>
       </h1>
     </main>
   );
